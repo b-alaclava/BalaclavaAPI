dba9fece4b79bfa94ec2532a5a9c5b5c5a889d23
no-ty <b.svg@icloud.com>
2024-08-23 02:30:23 -0400

    test

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -6,6 +6,7 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 public class ExamplePluginTest {
     public static void main(String[] args) throws Exception {
         ExternalPluginManager.loadBuiltin();
+        
         RuneLite.main(args);
     }
 }
