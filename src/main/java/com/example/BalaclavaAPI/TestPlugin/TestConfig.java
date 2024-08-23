eba46a1c763f9e0802f3856675b4b3c01ce4277a
no-ty <b.svg@icloud.com>
2024-08-23 13:36:15 -0400

    fix things

--- src/main/java/com/example/BalaclavaAPI/TestPlugin/TestConfig.java
+++ src/main/java/com/example/BalaclavaAPI/TestPlugin/TestConfig.java
@@ -7,16 +7,5 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("Csuperheat")
 public interface TestConfig extends Config {
 
-    @ConfigItem(
-            keyName = "bar",
-            name = "Bar",
-            description = "Bar to create with superheat"
-    )
-    default Bar bar() {
-        return Bar.GOLD_BAR;
-    }
-
-
-
 
 }
